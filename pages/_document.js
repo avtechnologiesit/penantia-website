2df90537da675a3c9406a77ad251604c16d71891
avtechnologiesit <98891888+avtechnologiesit@users.noreply.github.com>
2026-06-24 12:43:06 +0530

    update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,19 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;700&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#1A1A18" />
+        <meta name="theme-color" content="#1a1a18" />
       </Head>
-      <body><Main /><NextScript /></body>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
     </Html>
   )
 }
